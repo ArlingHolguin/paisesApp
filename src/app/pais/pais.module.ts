94d67067a4aee5e96701e6aa6908f4e1f20a87dc
Arling Holguin <arlingholguin@gmail.com>
2022-11-28 09:55:14 -0500

    Ajustes componentes

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
 import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+import { PaisCardComponent } from './components/pais-card/pais-card.component';
+import { PaisInputComponent } from './components/pais-input/pais-input.component';
 
 
 
@@ -12,16 +16,23 @@ import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
     PorCapitalComponent,
     PorPaisComponent,
     PorRegionComponent,
-    VerPaisComponent
+    VerPaisComponent,
+    PaisCardComponent,
+    PaisInputComponent
   ],
   exports:[
     PorCapitalComponent,
     PorPaisComponent,
     PorRegionComponent,
-    VerPaisComponent
+    VerPaisComponent,
+    PaisCardComponent,
+    PaisInputComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule
+
   ]
 })
 export class PaisModule { }
